50f00fade26ce21fa56a6402d99ac8c4e7cc5acd
Liming Chang <lmchang@ms10.hinet.net>
2022-04-04 14:24:33 +0800

    ATM add shared_pref function

--- app/src/main/java/com/liming/atm/LoginActivity.kt
+++ app/src/main/java/com/liming/atm/LoginActivity.kt
@@ -12,18 +12,32 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        val userid = getSharedPreferences("atm",MODE_PRIVATE)   // 檔名為 atm.xml, 權限僅限於本程式內存取
+            .getString("PREF_USERID","")            // 讀取 PREF_USERID的內容
+        val passwd = getSharedPreferences("atm",MODE_PRIVATE)
+            .getString("PREF_PASSWD","")            // 讀取 PREF_PASSWD的內容
+        ed_userid.setText(userid)
+        ed_passwd.setText(passwd)
     }
     fun login(view: View){
         val userid = ed_userid.text.toString()
         val passwd = ed_passwd.text.toString()
         if(userid == "liming" && passwd == "1234"){
+            // 把 user id & password存入 shared_prefs 目錄的 atm.xml檔案中
+            getSharedPreferences("atm",MODE_PRIVATE)
+                .edit()    // 打開 editor, 可以開始編輯內容
+                .putString("PREF_USERID",userid)  // 存入字串信息
+                .putString("PREF_PASSWD",passwd)
+                .putInt("INT",123)
+                .apply()   // apply 以上的資料
+
+            // 顯示 toast 信息
             Toast.makeText(this,"登入成功", Toast.LENGTH_LONG).show()
 
             // 以下三行是回傳信息給第一個視窗
-            intent.putExtra("LOGIN_USERID",userid)
-            intent.putExtra("LOGIN_PASSWD",passwd)
-            setResult(Activity.RESULT_OK,intent)
+            intent.putExtra("LOGIN_USERID",userid)   // 把 user id 放入intent
+            intent.putExtra("LOGIN_PASSWD",passwd)   // 把 pass word放入 intent
+            setResult(Activity.RESULT_OK,intent)           // 設定回傳值為 RESULT_OK, 並傳出intent值
             // 結束視窗
             finish()
         }else{
---
+++ app/src/main/java/com/liming/atm/MaterialActivity.kt
@@ -0,0 +1,28 @@
+package com.liming.atm
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.liming.atm.databinding.ActivityMaterialBinding
+
+class MaterialActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMaterialBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMaterialBinding.inflate(layoutInflater)   // 建立一個ActivityMaterial的binding
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
