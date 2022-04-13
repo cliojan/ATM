2048cd667d42a8f61ff4f5841c0dab3ce8ee930f
Liming Chang <lmchang@ms10.hinet.net>
2022-04-13 20:34:32 +0800

    Add OkHttp & orgJSON

--- app/src/main/java/com/liming/atm/TransActivity.kt
+++ app/src/main/java/com/liming/atm/TransActivity.kt
@@ -6,19 +6,52 @@ import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
 import java.io.BufferedReader
 import java.net.URL
 
 class TransActivity : AppCompatActivity() {
+    companion object {
+        val TAG = TransActivity::class.java.simpleName
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trans)
+        val client = OkHttpClient.Builder()
+            .build()
+        val request = Request.Builder()
+            .url("https://atm201605.appspot.com/h")
+            .build()
         CoroutineScope(Dispatchers.IO).launch {
+             var response = client.newCall(request).execute()
+             response.body?.run{
+             //    Log.d("Liming",string())
+                 val json = string()
+                 parseJSON(json)
+             }
+            /*
             val reader = URL("https://atm201605.appspot.com/h")
                 .openConnection()
                 .getInputStream().bufferedReader()
             val json = reader.use(BufferedReader::readText)
             Log.d("Liming",json)
+             */
+        }
+    }
+    private fun parseJSON(json: String) {
+        val trans = mutableListOf<Transaction>()
+        val array = JSONArray(json)
+        for (i in 0 until array.length()){
+            val obj = array.getJSONObject(i)
+            val account = obj.getString("account")
+            val date = obj.getString("date")
+            val amount = obj.getInt("amount")
+            val type = obj.getInt("type")
+            val t = Transaction(account,date,amount,type)
+            Log.d("Liming",t.toString())
+            trans.add(t)
         }
     }
 }
---
+++ app/src/main/java/com/liming/atm/Transaction.kt
@@ -0,0 +1,13 @@
+package com.liming.atm
+
+data class Transaction(
+    var account:String,
+    var date: String,
+    var amount: Int,
+    var type:Int
+){
+    constructor():this("","",0,0)
+    override fun toString():String {
+        return "$account $date $amount $type"
+    }
+}
